bd93ed35368955e5569cc9278aad8b37e3f5b679
Kevan Haggins <63670071+simplesapien@users.noreply.github.com>
2022-08-26 21:14:05 -0400

    Refactored to Class
    Refactored this to a Class set up as per an assignment. Also got rid of the star rating, it was just pretty disorganized and felt a little unnecessary.

--- scripts/library.js
+++ scripts/library.js
@@ -1,127 +1,74 @@
-// Form variables
+// Cache DOM elements
+const addBook = document.querySelector('#add-book');
+const libraryDisplay = document.querySelector('.library');
+const closeForm = document.querySelector('.modal-close');
 const formModal = document.querySelector('.form-modal');
 const formContainer = document.querySelector('.form-container');
 const form = document.querySelector('form');
 
-// Create an array to track the stars in script
-const stars = [];
-for (i = 1; i < 6; i++) {
-    stars.push(document.querySelector(`.form-star${i}`))
-};
 
-// Add an event listener to each star, and change the src based on which star is hovered over, or clicked
-let rating;
-let regex = /\d+/;
+class Library {
 
-stars.forEach(element => {
+    displayForm() {
+        formModal.classList.remove('fadeOut');
+        formModal.classList.add('fadeIn');
+        formContainer.classList.add('fadeIn');
+        addBook.classList.remove('fadeIn');
+        addBook.classList.add('fadeOut');
+    }
 
-    element.addEventListener('mouseover', function () {
+    createNewBook() {
+        const bookName = document.querySelector('#book').value;
+        const author = document.querySelector('#author').value;
+        let newBook = new Book(bookName, author);
+        newBook.addToDom();
+    }
 
-        // Parse out the star number that is being hovered over
-        let num = element.className.match(regex);
-        for (let i = 0; i < num; i++) {
-            stars[i].src = 'img/star.svg'
-        }
-    });
+    resetForm() {
+        formModal.classList.remove('fadeIn');
+        formModal.classList.add('fadeOut');
+        formContainer.classList.remove('fadeIn');
+        formContainer.classList.add('fadeOut');
+        addBook.classList.remove('fadeOut');
+        addBook.classList.add('fadeIn');
+        form.reset();
+    }
 
-    element.addEventListener('click', function () {
+}
 
-        let num = element.className.match(regex);
-        for (let i = 0; i < num; i++) {
-            stars[i].src = 'img/star.svg'
-        };
-        rating = num;
-    });
+class Book {
 
-    // If no star has been clicked, revert all stars to the outline version on mouse out. If there has been a star clicked, only change the src of the stars back to an outline if they are past that clicked star
-    element.parentNode.addEventListener('mouseout', function () {
-        if (!rating) element.src = 'img/staroutline.svg';
-        else {
-            for (let i = rating; i < stars.length; i++) {
-                stars[i].src = 'img/staroutline.svg';
-            }
-        }
-    });
-});
+    constructor(book, author) {
+        this.book = book;
+        this.author = author;
+    }
 
-// Controls
-const addBook = document.querySelector('#add-book');
-const closeForm = document.querySelector('.modal-close');
+    addToDom() {
 
-// Library array + display library
-const library = document.querySelector('.library');
+        const entryContainer = document.createElement('div');
+        const entryTitle = document.createElement('h1');
+        entryTitle.innerHTML = this.book;
+        const entryAuthor = document.createElement('h2');
+        entryAuthor.innerHTML = this.author;
+        entryContainer.append(entryTitle, entryAuthor);
+        entryContainer.className = 'library-item';
+        libraryDisplay.prepend(entryContainer);
 
-// Display form when the add button is clicked
-addBook.addEventListener('click', function () {
-    formModal.classList.remove('fadeOut');
-    formModal.classList.add('fadeIn');
-    formContainer.classList.add('fadeIn');
-    addBook.classList.remove('fadeIn');
-    addBook.classList.add('fadeOut');
-});
+    }
 
+}
+
+// Create the library object
+const library = new Library;
+
+// Bind events
+addBook.addEventListener('click', library.displayForm);
+formModal.addEventListener('click', function (event) {
+    if (!event.target.closest('.form-container')) library.resetForm();
+})
+closeForm.addEventListener('click', library.resetForm);
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    addToLibrary();
-    resetForm();
+    library.createNewBook();
+    library.resetForm();
 });
-
-// Close the modal+form if the user clicks it or the "X" in the corner
-formModal.addEventListener('click', function (event) {
-    if (!event.target.closest('.form-container')) resetForm();
-})
-closeForm.addEventListener('click', resetForm);
-
-// Instantiate book class
-function Book(book, author, rating) {
-    this.book = book;
-    this.author = author;
-    this.rating = rating;
-}
-
-function resetForm() {
-    formModal.classList.remove('fadeIn');
-    formModal.classList.add('fadeOut');
-    formContainer.classList.remove('fadeIn');
-    formContainer.classList.add('fadeOut');
-    addBook.classList.remove('fadeOut');
-    addBook.classList.add('fadeIn');
-    stars.forEach(element => {
-        element.src = 'img/staroutline.svg';
-        rating = 0;
-    });
-    form.reset();
-}
-
-// Function to add stars to the library item based on rating provided by user
-function addStars(rating) {
-    const stars = document.createElement('div');
-    stars.classList.add = 'library-stars';
-    for (let i = 0; i < rating; i++) {
-        const star = document.createElement('img');
-        star.src = 'img/starblack.svg';
-        stars.append(star);
-    }
-    return stars;
-}
-
-function addToLibrary() {
-
-    // Create a new instance of the Book class and save it in the myLibrary array
-    const book = document.querySelector('#book').value;
-    const author = document.querySelector('#author').value;
-    const starRating = rating;
-    const entry = new Book(book, author, starRating);
-
-    // Create the display for that specific library item and add it to the display
-    const entryContainer = document.createElement('div');
-    const entryTitle = document.createElement('h1');
-    entryTitle.innerHTML = entry.book;
-    const entryAuthor = document.createElement('h2');
-    entryAuthor.innerHTML = entry.author;
-    const entryRating = addStars(rating);
-    entryContainer.append(entryTitle, entryAuthor, entryRating);
-    entryContainer.className = 'library-item';
-    entryContainer.className.add = 'fadeIn';
-    library.prepend(entryContainer);
-}
